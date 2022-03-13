58c126b8550d3dc7a69f42fd9dd7c8cc4d5668d7
nerderlyne <shivanshityagi@icloud.com>
2022-03-14 02:47:55 +0530

    update logo

--- components/Kali.js
+++ components/Kali.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { styled } from "@stitches/react";
-
+import logo from "../public/img/logo-minimal.png";
 const LogoContainer = styled("div", {
   padding: "10px",
 });
@@ -9,12 +9,7 @@ const LogoContainer = styled("div", {
 export default function Kali() {
   return (
     <LogoContainer>
-      <Image
-        src="/../public/img/logo-minimal.png"
-        alt="KALI logo"
-        height="100px"
-        width="62px"
-      />
+      <Image src={logo.src} alt="KALI logo" height="100px" width="62px" />
     </LogoContainer>
   );
 }
