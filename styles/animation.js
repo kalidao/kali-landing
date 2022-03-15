1ba550aa38e26934d0b000c67e9e6ba040fe7fc6
nerderlyne <shivanshityagi@icloud.com>
2022-03-16 02:29:34 +0530

    export animation

--- styles/animation.js
+++ styles/animation.js
@@ -1,41 +1,41 @@
 import { keyframes } from "@stitches/react";
 
-const enterFromRight = keyframes({
+export const enterFromRight = keyframes({
   from: { transform: "translateX(200px)", opacity: 0 },
   to: { transform: "translateX(0)", opacity: 1 },
 });
 
-const enterFromLeft = keyframes({
+export const enterFromLeft = keyframes({
   from: { transform: "translateX(-200px)", opacity: 0 },
   to: { transform: "translateX(0)", opacity: 1 },
 });
 
-const exitToRight = keyframes({
+export const exitToRight = keyframes({
   from: { transform: "translateX(0)", opacity: 1 },
   to: { transform: "translateX(200px)", opacity: 0 },
 });
 
-const exitToLeft = keyframes({
+export const exitToLeft = keyframes({
   from: { transform: "translateX(0)", opacity: 1 },
   to: { transform: "translateX(-200px)", opacity: 0 },
 });
 
-const scaleIn = keyframes({
+export const scaleIn = keyframes({
   from: { transform: "rotateX(-30deg) scale(0.9)", opacity: 0 },
   to: { transform: "rotateX(0deg) scale(1)", opacity: 1 },
 });
 
-const scaleOut = keyframes({
+export const scaleOut = keyframes({
   from: { transform: "rotateX(0deg) scale(1)", opacity: 1 },
   to: { transform: "rotateX(-10deg) scale(0.95)", opacity: 0 },
 });
 
-const fadeIn = keyframes({
+export const fadeIn = keyframes({
   from: { opacity: 0 },
   to: { opacity: 1 },
 });
 
-const fadeOut = keyframes({
+export const fadeOut = keyframes({
   from: { opacity: 1 },
   to: { opacity: 0 },
 });
