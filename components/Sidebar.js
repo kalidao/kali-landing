c5651a78001fb176b40bd818d2f51489271da16e
Shiv <shivanshityagi@icloud.com>
2022-04-15 19:48:14 +0530

    fix social links
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,8 +5,8 @@ export default function Sidebar() {
   return <SidebarLayout>
     <SidebarSocial>
       <SocialLink icon={<MirrorIcon />} link="https://mirror.xyz/kalico.eth" />
-      <SocialLink icon={<DiscordIcon />} link="https://discord.gg/qWVRw4StaC" />
       <SocialLink icon={<TwitterIcon />} link="https://twitter.com/_KaliDAO" />
+      <SocialLink icon={<DiscordIcon />} link="https://discord.gg/qWVRw4StaC" />
       <SocialLink icon={<GitHubIcon />} link="https://github.com/kalidao/" />
     </SidebarSocial>
   </SidebarLayout>
