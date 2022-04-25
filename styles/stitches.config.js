1d7406d4f06404b1cb63551a05c49e45adf541f6
Shiv <shivanshityagi@icloud.com>
2022-04-25 19:16:35 +0530

    nits to style configs
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- styles/stitches.config.js
+++ styles/stitches.config.js
@@ -3,14 +3,15 @@ import { createStitches } from "@stitches/react";
 export const { styled } = createStitches({
   theme: {
     colors: {
-      black: "rgba(19, 19, 21, 1)",
-      white: "rgba(255, 255, 255, 1)",
-      gray: "rgba(243, 243, 243, 0.6)",
-      red: "rgba(208, 3, 3, 0.85)",
-      green: "rgba(71, 115, 0, 0.95)",
+      black: "#000000",
+      white: "#FFFFFF",
+      lightgray: "#F2F2F2",
+      darkgray: "#BDBDBD",
+      red: "#FD2323",
+      green: "#2EC616",
+      yellow: "#FDCC6E",
       purple: "#7000FF",
-      yellow: "rgba(255, 215, 0, 0.95)",
-      gold: "rgba(232, 186, 0, 1)",
+      border: "#DDDDDD",
       blackAlpha: "rgba(19, 19, 21, 0.98)",
     },
     media: {
