4bbaa48b6d077b5e6db5bb945eb46b2521983fca
Shiv <shivanshityagi@icloud.com>
2022-05-19 22:35:53 +0530

    add ssr css
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,41 @@
-import Head from "next/head";
-import Image from "next/image";
-import { styled } from "../styles/stitches.config";
-import Sidebar from "../components/Sidebar";
-import Header from "../components/Header";
-import MainLanding from "../components/MainLanding"
-import Features from "../components/Features";
-import background from "../public/img/kali-bg.png";
+import Head from 'next/head'
+import Image from 'next/image'
+import { styled } from '../styles/stitches.config'
+import Sidebar from '../components/Sidebar'
+import Header from '../components/Header'
+import MainLanding from '../components/MainLanding'
+import Features from '../components/Features'
+import background from '../public/img/kali-bg.png'
+import { getCssText } from '../styles/stitches.config'
 
-const Layout = styled("div", {
-  minHeight: "100vh",
-  background: "#fff",
-  position: "relative"
-});
+const Layout = styled('div', {
+  minHeight: '100vh',
+  background: '#fff',
+  position: 'relative',
+})
 
 const ImageBox = styled('div', {
-  pointerEvents: "none",
-  zIndex: "-100"
-});
+  pointerEvents: 'none',
+  zIndex: '-100',
+})
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>KALI</title>
-      <meta property="og:title" content="KALI" key="title" />
-      <meta name="viewport" content="width=800" />
-    </Head>
-    <Layout>
-      <Header />
-      <Sidebar />
-      <MainLanding />
-      <ImageBox>
-        <Image src={background.src} layout="fill" priority/>
-      </ImageBox>
-    </Layout>
+      <Head>
+        <title>KALI</title>
+        <meta property="og:title" content="KALI" key="title" />
+        <meta name="viewport" content="width=800" />
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      </Head>
+      <Layout>
+        <Header />
+        <Sidebar />
+        <MainLanding />
+        <ImageBox>
+          <Image src={background.src} layout="fill" priority />
+        </ImageBox>
+      </Layout>
     </>
-  );
+  )
 }
