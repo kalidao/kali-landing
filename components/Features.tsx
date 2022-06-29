97dd994d50f4635ab0c9091cca774aa298937418
Shivanshi Tyagi <shivanshityagi@icloud.com>
2022-06-29 16:41:01 +0530

    temp remove
    Signed-off-by: Shivanshi Tyagi <shivanshityagi@icloud.com>

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,47 +1,10 @@
 import React from 'react'
-import { styled } from '../styles/stitches.config'
+import { Box, Cards, FeatureCard } from '../styles/features'
 import { features } from '../constants/features'
-
-const Box = styled('div', {
-  display: 'grid',
-  margin: '0rem 0rem 0rem 0rem',
-  zIndex: '10',
-  pointerEvents: 'none',
-})
-
-const Cards = styled('div', {
-  display: 'flex',
-  gap: '1rem',
-})
-
-const Card = styled('div', {
-  maxHeight: '25vh',
-  minWidth: '10rem',
-  color: '$foreground',
-})
-
-const Title = styled('div', {
-  fontWeight: '800',
-  fontSize: '16px',
-  paddingBottom: '1rem',
-})
-
-const Description = styled('div', {})
-
-const FeatureCard = ({ title, image, description }) => {
-  return (
-    <Card>
-      <img src={image} />
-      <Title>{title}</Title>
-      <Description>{description}</Description>
-    </Card>
-  )
-}
 
 function Features() {
   return (
     <Box>
-      {/* <Heading>Features</Heading> */}
       <Cards>
         {features &&
           features.map((feature, index) => (
