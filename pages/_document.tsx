96885e1cf732d671e2e35aeae3a47851e2825e23
Shivanshi Tyagi <shivanshityagi@icloud.com>
2022-07-08 20:57:03 +0530

    update
    Signed-off-by: Shivanshi Tyagi <shivanshityagi@icloud.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,29 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+          <link
+            rel="preload"
+            href="/fonts/Px-Grotesk-Regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+                @font-face {
+                  font-family: 'Regular';
+                  font-weight: 400;
+                  font-display: swap;
+                  src: url(/fonts/Px-Grotesk-Regular.woff2) format('woff2');
+                }
+                `,
+            }}
+          />
         </Head>
-        <body>
+        <body style={{
+            background: 'hsl(0, 0%, 4.5%)',
+          }}>
           <Main />
           <NextScript />
         </body>
